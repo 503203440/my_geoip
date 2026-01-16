e5b931d78ece4ecdde7308f569f177dbbf7a38d4
YX <yx503203440@gmail.com>
2026-01-16 11:26:53 +0800

    增加了目录说明，修改启动脚本

--- start.cmd
+++ start.cmd
@@ -1,3 +1,3 @@
-set ASN_DB_PATH=C:/Users/50320/Downloads/GeoLite2-ASN_20260114/GeoLite2-ASN.mmdb
-set CITY_DB_PATH=C:/Users/50320/Downloads/GeoLite2-City_20260113/GeoLite2-City.mmdb
+set ASN_DB_PATH=./geoip_database/GeoLite2-ASN.mmdb
+set CITY_DB_PATH=./geoip_database/GeoLite2-City.mmdb
 bun run index.ts
